dbc297e460cabb7126a915f19148611c00e5c2d1
George_M <msgoha555@yandex.ru>
2022-06-05 20:57:17 +0300

    HW 43 completed

---
+++ Homework_43/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите b1 =>");
+int b1 = int.Parse(Console.ReadLine());
+Console.Write("Введите k1 =>");
+int k1 = int.Parse(Console.ReadLine());
+Console.Write("Введите b2 =>");
+int b2 = int.Parse(Console.ReadLine());
+Console.Write("Введите k2 =>");
+int k2 = int.Parse(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+Console.WriteLine($"Координаты точки пересечения = {x}, {y}");
