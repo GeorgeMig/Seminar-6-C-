48384da6d7d48ef2c4a481f35cc10fe510adfc50
George_M <msgoha555@yandex.ru>
2022-06-05 19:36:41 +0300

    Completed homework_41

--- Homework_41/Program.cs
+++ Homework_41/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Write("Введите отрицательные или положительные через пробел: ");
+int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array [i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine ($"Количество чисел больше нуля, введенных пользователем = {count}");
